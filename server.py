814ea960011b35b24636f9ed05af8970be3878eb
Chaoqi ZHANG <prncoprs@163.com>
2019-08-27 11:01:56 +0800

    add face detection

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ r = redis.Redis('localhost', 6379)
 frame_width = 320
 frame_height = 240
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 for i in range(0, 80):
     frameBuf = r.lindex(VpxFrameKey, i)
     frame = np.zeros(frame_width, dtype=np.uint8)
@@ -17,8 +19,18 @@ for i in range(0, 80):
         frameTmp = np.frombuffer(frameBuf, dtype=np.uint8, count = frame_width, offset = frame_width * h)
         frame = np.row_stack((frame, frameTmp))
     frame = np.delete(frame, 0, 0)
-
     gray = cv2.cvtColor(frame, cv2.COLOR_YUV2GRAY_420)
+
+    face_reg = True
+    try: 
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    except :
+        print("no face reg")
+        face_reg = False
+    if face_reg :
+        for(x,y,w,h) in faces:
+            frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        
     cv2.imshow("frame", frame)
     cv2.waitKey(10)
 
